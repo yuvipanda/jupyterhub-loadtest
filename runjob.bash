4b1c0738cb4a5b07d7a6be67353fff025c950f4d
yuvipanda <yuvipanda@gmail.com>
2017-08-02 13:19:54 -0700

    Fix pod.yaml to be actually a pod

--- runjob.bash
+++ runjob.bash
@@ -5,6 +5,6 @@ COUNT=${2}
 DELAY=${3}
 CONNECT_IP=${4}
 for i in $(seq 1 ${RUNS}); do
-    cat job.yaml | sed "s/CONNECT_IP/${CONNECT_IP}/" | sed "s/NUM/${i}/" | sed "s/RUN_COUNT/${COUNT}/" | kubectl apply -f -
+    cat pod.yaml | sed "s/CONNECT_IP/${CONNECT_IP}/" | sed "s/NUM/${i}/" | sed "s/RUN_COUNT/${COUNT}/" | kubectl apply -f -
     sleep ${DELAY}
 done
