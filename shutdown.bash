17b26eb8b33db7c58cc6a8e61cbb3c3c210c8897
Christian Moscardi <moscardi79@gmail.com>
2017-08-03 14:50:59 -0400

    shutdown using labels instead of grep

--- shutdown.bash
+++ shutdown.bash
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 echo "stopping test runs..."
-for pod in `kubectl get pods | grep 'j-[a-z]-[0-9]-[a-z]' | cut -d " " -f 1`; 
-do
-  kubectl delete pod $pod;
-done
+kubectl delete pod -l heritage=jupyterhub-loadtest;
 
 echo "deleting jupyter servers..."
-for pod in `kubectl get pods | grep "jupyter-j" | cut -d " " -f 1`;
-do
-  kubectl delete pod $pod;
-done
+kubectl delete pod -l heritage=jupyterhub;
