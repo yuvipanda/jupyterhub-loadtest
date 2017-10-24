6bfbe0e7de46addff0d5c3503719955b926cd36d
yuvipanda <yuvipanda@gmail.com>
2017-10-23 21:49:44 -0700

    Clean up to suck a lot less
    - Better understanding of the limiations of async / await
      lead to code that actually executes in the order it should
      (hopefully)
    - Stop the kernel and server after a given time period
    - Don't count startKernel as done until the kernel has connected

--- stress.js
+++ stress.js
@@ -83,71 +83,138 @@ class User {
         return false;
     };
 
-    async startKernel() {
+    async stopServer() {
         let startTime = process.hrtime();
+        let stopUrl = this.hubUrl + '/hub/api/users/' + this.username + '/server';
         let headers = {
-            'Cookie': this.cookieJar.getCookieString(this.notebookUrl + '/')
+            'Referer': this.hubUrl + '/hub'
         };
-        this.cookieJar.getCookies(this.notebookUrl).forEach((cookie) => {
-            if (cookie.key == '_xsrf') { headers['X-XSRFToken'] = cookie.value; };
-        });
-
-        let serverSettings = services.ServerConnection.makeSettings({
-            xhrFactory: function () { return new xhr.XMLHttpRequest(); },
-            wsFactory: function (url, protocol) {
-                return new ws(url, protocol, {'headers': headers});
-            },
-            requestHeaders: headers,
-            baseUrl: this.notebookUrl
-        });
-
         try {
-            let kernelSpecs = await services.Kernel.getSpecs(serverSettings);
-            this.kernel = await services.Kernel.startNew({
-                name: kernelSpecs.default,
-                serverSettings: serverSettings
+            let resp = await request({
+                method: 'DELETE',
+                url: stopUrl,
+                jar: this.cookieJar,
+                resolveWithFullResponse: true,
+                headers: headers
+            });
+            let timeTaken = process.hrtime(startTime);
+            this.emitEvent('server-stop.success', {'duration': timeTaken[0] * 1000 + timeTaken[1] / 1000000});
+        } catch(e) {
+            let timeTaken = process.hrtime(startTime);
+            this.emitEvent('server-stop.failure', {'duration': timeTaken[0] * 1000 + timeTaken[1] / 1000000});
+        }
+
+    }
+
+    startKernel() {
+        return new Promise((resolve, reject) => {
+            let startTime = process.hrtime();
+            let headers = {
+                'Cookie': this.cookieJar.getCookieString(this.notebookUrl + '/')
+            };
+            this.cookieJar.getCookies(this.notebookUrl).forEach((cookie) => {
+                if (cookie.key == '_xsrf') { headers['X-XSRFToken'] = cookie.value; };
+            });
+
+            let serverSettings = services.ServerConnection.makeSettings({
+                xhrFactory: function () { return new xhr.XMLHttpRequest(); },
+                wsFactory: function (url, protocol) {
+                    return new ws(url, protocol, {'headers': headers});
+                },
+                requestHeaders: headers,
+                baseUrl: this.notebookUrl
+            });
+
+            let failure = () => {
+                let timeTaken = process.hrtime(startTime);
+                this.emitEvent('kernel-start.failure', {'duration': timeTaken[0] * 1000 + timeTaken[1] / 1000000});
+                reject();
+            };
+            services.Kernel.getSpecs(serverSettings).then((kernelSpecs) => {
+                this.kernel = services.Kernel.startNew({
+                    name: kernelSpecs.default,
+                    serverSettings: serverSettings
+                }).then((kernel) => {
+
+                    this.kernel = kernel;
+                    this.kernel.statusChanged.connect((status) => {
+                        if (status.status == 'connected') {
+                            resolve();
+                        }
+                    });
+                });
             });
             let timeTaken = process.hrtime(startTime);
             this.emitEvent('kernel-start.success', {'duration': timeTaken[0] * 1000 + timeTaken[1] / 1000000});
-        } catch(e) {
+        });
+    }
+
+    async stopKernel() {
+        let startTime = process.hrtime();
+        if (this.kernel) {
+            await this.kernel.shutdown();
             let timeTaken = process.hrtime(startTime);
-            this.emitEvent('kernel-start.failure', {'duration': timeTaken[0] * 1000 + timeTaken[1] / 1000000});
+            this.emitEvent('kernel-stop.success', {'duration': timeTaken[0] * 1000 + timeTaken[1] / 1000000});
         }
     }
 
-    async executeCode() {
-        let executeFib = ()=> {
-            let future = this.kernel.requestExecute({ code: 'fib = lambda n: n if n < 2 else fib(n-1) + fib(n-2); print(fib(20))'} );
-            // This will fire if we don't have an answer back from the kernel within 1s
-            let startTime = process.hrtime();
-            let failureTimer = setTimeout(() => {
-                let timeTaken = process.hrtime(startTime);
-                this.emitEvent('code-execute.timeout', {duration: timeTaken[0] * 1000 + timeTaken[1] / 1000000});
-            }, 1000);
-            future.onIOPub = (msg) => {
-                clearTimeout(failureTimer);
-                let timeTaken = process.hrtime(startTime);
-                if (msg.content.text == '6765\n') {
-                    setTimeout(executeFib, 1000);
-                    this.emitEvent('code-execute.success', {duration: timeTaken[0] * 1000 + timeTaken[1] / 1000000});
+    executeCode(timeout) {
+        // Explicitly *not* an async defined function, since we want to
+        // explictly return a Promise.
+        let cancelled = false;
+
+        setTimeout(() => { cancelled = true; }, timeout);
+        return new Promise((resolve, reject) => {
+
+            let executeFib = () => {
+                if (cancelled) {
+                    this.emitEvent('code-execute.complete', {});
+                    resolve();
+                    return;
                 }
+                let future = this.kernel.requestExecute({ code: 'fib = lambda n: n if n < 2 else fib(n-1) + fib(n-2); print(fib(20))'} );
+                // This will fire if we don't have an answer back from the kernel within 1s
+                let startTime = process.hrtime();
+                let failureTimer = setTimeout(() => {
+                    let timeTaken = process.hrtime(startTime);
+                    this.emitEvent('code-execute.timeout', {duration: timeTaken[0] * 1000 + timeTaken[1] / 1000000});
+                    reject();
+                }, 1000);
+                future.onIOPub = (msg) => {
+                    clearTimeout(failureTimer);
+                    let timeTaken = process.hrtime(startTime);
+                    if (msg.content.text == '6765\n') {
+                        setTimeout(executeFib, 1000);
+                        this.emitEvent('code-execute.success', {duration: timeTaken[0] * 1000 + timeTaken[1] / 1000000});
+                    }
+                };
             };
-        };
-        executeFib();
+            executeFib();
+        });
     }
 
 }
 
 function main(hubUrl, userCount, userPrefix, jitter) {
 
-    function launch(i) {
+    async function launch(i) {
+
+        // Wait for a random amount of time before actually launching
+        await new Promise(r => setTimeout(r, Math.random() * jitter));
+
         let u = new User(hubUrl, userPrefix + String(i), 'wat');
-        u.login().then(() => u.startServer()).then(() => u.startKernel()).then(() => u.executeCode());
-        if (i < userCount) {
-            setTimeout(function() { launch(i + 1);}, Math.random() * jitter);
-        }
+        await u.login();
+        await u.startServer();
+        await u.startKernel();
+        await u.executeCode(5000);
+        await u.stopKernel();
+        await u.stopServer();
+
     }
-    launch(0);
+
+    for(let i=0; i < userCount; i++) {
+        launch(i);
+    }
 }
 
 program
